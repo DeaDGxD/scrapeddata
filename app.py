29440c432349915ba692a18fab2d37c058f0d022
DeaDGxD <59544724+DeaDGxD@users.noreply.github.com>
2020-02-24 20:51:10 -0600

    finished

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
-conn = sqlite3.connect('Temp.db')
-c = conn.cursor()
-c.execute('select * from weatherdata')
-data = c.fetchall()
 
 @app.route('/')
 def hello_world():
+    conn = sqlite3.connect('Temp.db')
+    c = conn.cursor()
+    c.execute('select * from weatherdata')
+    data = c.fetchall()
     return render_template('table.html', data=data)
 
 
